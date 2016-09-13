ec5e67e6dbfab268297b3b4b897fc6e0ad290e08
Philippe Dionne <dionne.phil@gmail.com>
2016-09-13 13:56:38 -0400

    Cleanup

--- config.rb
+++ config.rb
@@ -32,13 +32,9 @@ end
 configure :build do
   ignore 'stylesheets/components/*'
 
-  # Minify CSS on build
-  activate :minify_css
-
-  # Minify Javascript on build
-  activate :minify_javascript
-
-  activate :minify_html
+  activate :minify_css          # Minify CSS on build
+  activate :minify_javascript   # Minify Javascript on build
+  activate :minify_html         # Minify HTML on build
 
   activate :gzip
 end
